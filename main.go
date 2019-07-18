dac97edcacfa8db742878cc4f98f398e8d9cd854
tujiaw <tujiaw@163.com>
2019-07-18 10:36:20 +0800

    add

--- main.go
+++ main.go
@@ -8,10 +8,20 @@ import (
 )
 
 type WebhookHandler struct {
-	monitorList []string
+	monitor map[string]func()
 }
 
-func (handler *WebhookHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func NewWebhookHandler() *WebhookHandler {
+	return &WebhookHandler{
+		monitor: make(map[string]func()),
+	}
+}
+
+func (webhook *WebhookHandler) AddHandler(fullname string, handler func()) {
+	webhook.monitor[fullname] = handler
+}
+
+func (webhook *WebhookHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	bbody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Fprint(w, err)
@@ -23,18 +33,31 @@ func (handler *WebhookHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)
 		fmt.Fprint(w, err)
 		return
 	}
-	rep := payload["repository"].(map[string]interface{})
-	fmt.Println(rep["full_name"])
+	repository := payload["repository"].(map[string]interface{})
+	fullname := repository["full_name"].(string)
+	if webhook.monitor[fullname] != nil {
+		webhook.monitor[fullname]()
+		fmt.Fprint(w, "success")
+	} else {
+		fmt.Fprint(w, "not handler")
+	}
 }
 
 func main() {
-	const ADDRESS = ":3000"
+	webhook := NewWebhookHandler()
+	webhook.AddHandler("tujiaw/webhookgo", func() {
+		fmt.Println("1111111111111111111")
+	})
+	webhook.AddHandler("tujiaw/ningtogo", func() {
+		fmt.Println("22222222222222222")
+	})
 
-	http.Handle("/webhook", &WebhookHandler{})
+	http.Handle("/webhook", webhook)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "success")
 	})
 
+	const ADDRESS = ":3000"
 	fmt.Println("start address", ADDRESS)
 	err := http.ListenAndServe(ADDRESS, nil)
 	if err != nil {
